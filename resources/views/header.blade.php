ac0591b9974d4abf6b850dc9c077e36ee7595f82
Pedro Pimentel <pedrostm2@outlook.com>
2019-07-05 17:40:15 -0300

    blade

---
+++ resources/views/header.blade.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="estilo.css" crossorigin="anonymous">
+
+    <title>Bem Vindo ao Bora</title>
+  </head>
+  <body>
+    <div class="container">
+        <div class="row">
+          <div class="col">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                  <li class="nav-item active">
+                    <!-- <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a> -->
+                    <a class="nav-link text-white" href="index.html"><img src="img/logo.png" class="img-fluid" alt="Responsive image"></a>
+                    
+                  </li>
+                  <div class="alinhamento">
+                    <div class="row">
+                        <li class="nav-item text-right">
+                            <a class="nav-link text-white" href="#"> <h5>Passeios</h5></a>
+                        </li>
+                        <li class="nav-item text-right">
+                            <a class="nav-link text-white" href="escolha_login.html"> <h5>Login</h5></a>
+                        </li>
+                    </div>
+                  </div>
+                </ul>
+                <!-- <form class="form-inline my-2 my-lg-0">
+                  <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                </form> -->
+              </div>
+            </nav>
+          </div>
+        </div>

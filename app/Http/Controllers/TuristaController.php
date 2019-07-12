3bee063140641a012e48714550419f094ba26fca
Pedro Pimentel <pedrostm2@outlook.com>
2019-07-12 15:22:42 -0300

    passeio

--- app/Http/Controllers/TuristaController.php
+++ app/Http/Controllers/TuristaController.php
@@ -18,6 +18,14 @@ class TuristaController extends BaseController
         $last_name = $request->input('last_name');
         $email = $request->input('email');
         $senha = $request->input('senha');
+        $rg = $request->input('rg');
+        $cpf = $request->input('cpf');
+        $data_nasc = $request->input('data_nasc');
+        $genero = $request->input('genero');
+        $fone = $request->input('fone');
+        $img = $request->input('img');
+
+        
 
     }
 
